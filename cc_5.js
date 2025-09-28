e1e34dcd1e62ebeb0634b804c5df71e90669085d
Tristan Zybala <tzybala@usf.edu>
2025-09-28 19:01:24 -0400

    Overtime Pay function created to calculate employee overtime earnings

--- cc_5.js
+++ cc_5.js
@@ -13,7 +13,19 @@ console.log(employees);
 // Step 3
 function calculateBasePay(rate, hours) {
     return rate * hours
-}
+};
 
-let pay = calculateBasePay(40, 20.00);
-console.log(pay);
+let basePay = calculateBasePay(20.00, 40);
+console.log(basePay);
+
+// Step 4
+function calculateOvertimePay(rate, hours) {
+        if (hours > 40) {
+            return (1.5 * rate) * (hours - 40)
+        } else if (hours <= 40) {
+            return `No overtime found on record`
+        }
+};
+
+let overtimePay = calculateOvertimePay(20.00, 45);
+console.log(overtimePay);
