4ed034847163e5d2505e6f0a9173810888c8d445
Tristan Zybala <tzybala@usf.edu>
2025-09-28 18:31:47 -0400

    Created Employee Array

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Coding Challenge 5
+
+// Step 2
+let employees = [
+    {name: "Bob",hourlyRate: 20.00,hoursWorked: 40},
+    {name: "Bill",hourlyRate: 18.50,hoursWorked: 32},
+    {name: "Dale",hourlyRate: 22.50,hoursWorked: 40},
+    {name: "Randy",hourlyRate: 17.00,hoursWorked: 20}
+];
+
+console.log(employees);
